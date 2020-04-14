ef957cabe9eb042b9c2ea272f1c35faf784ab926
kqhaski <k1664032884@gmail.com>
2020-04-14 12:45:42 +0800

    version1.0

--- movies/tests.py
+++ movies/tests.py
@@ -4,14 +4,8 @@ from django.test import TestCase
 from .models import Movie
 
 for obj in Movie.objects.all():
-    if obj.cover and obj.is_valid(obj.cover):
-        print(f"{obj.name}链接有效")
-    else:
-        try:
-            obj.get_cover()
-        except:
-            print(f"*******{obj.name}图片获取失败******")
-            continue
-        else:
-            obj.save()
-            print(f"-------{obj.name}图片更新成功------")
+    if obj.cover and (not obj.is_valid(obj.cover)):
+        print(f"{obj.name}封面失效")
+        obj.cover = None
+        obj.save()
+        print(f"{obj.name}封面已删除")
